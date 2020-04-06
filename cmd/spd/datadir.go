519b2314aa531a90dbd431204414f432ecbf960d
Reinis Prikulis <reinis@prikulisdataservice.lv>
2020-04-06 18:54:55 +0300

    Rename old SiaPrime directory to ScPrime in AppData

---
+++ cmd/spd/datadir.go
@@ -0,0 +1,47 @@
+// datadir.go
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"gitlab.com/scpcorp/ScPrime/build"
+)
+
+// Check for Metadata directory presence and try to migrate if it does not
+// exist
+
+// dirExists checks if a directory exists.
+func dirExists(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
+}
+
+// migrateDataDir migrates existing metadata directory to new location
+func migrateDataDir() error {
+	fmt.Printf("Migrating metadata from %v to %v\n", defaultSiaPrimeDir(), build.DefaultMetadataDir())
+	return os.Rename(defaultSiaPrimeDir(), build.DefaultMetadataDir())
+}
+
+// defaultSiaPrimeDir returns the default data directory of older ScPrime nodes.
+// This method is used to migrate the metadata to the new default location.
+// The values for supported operating systems are:
+//
+// Linux:   $HOME/.siaprime
+// MacOS:   $HOME/Library/Application Support/SiaPrime
+// Windows: %LOCALAPPDATA%\SiaPrime
+func defaultSiaPrimeDir() string {
+	switch runtime.GOOS {
+	case "windows":
+		return filepath.Join(os.Getenv("LOCALAPPDATA"), "SiaPrime")
+	case "darwin":
+		return filepath.Join(os.Getenv("HOME"), "Library", "Application Support", "SiaPrime")
+	default:
+		return filepath.Join(os.Getenv("HOME"), ".siaprime")
+	}
+}
