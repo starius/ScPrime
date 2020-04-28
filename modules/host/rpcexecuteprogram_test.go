b0884634b2acf993e2eaa4b8182dfbcb75673861
Reinis Prikulis <reinis@prikulisdataservice.lv>
2020-04-28 15:04:34 +0300

    Adjust values for TestExecuteHasSectorProgram

--- modules/host/rpcexecuteprogram_test.go
+++ modules/host/rpcexecuteprogram_test.go
@@ -470,8 +470,8 @@ func TestExecuteHasSectorProgram(t *testing.T) {
 	// this particular program on the "renter" side. This way we can test that
 	// the bandwidth measured by the renter is large enough to be accepted by
 	// the host.
-	expectedDownload := uint64(5840) // download
-	expectedUpload := uint64(17520)  // upload
+	expectedDownload := uint64(7300) // download
+	expectedUpload := uint64(18980)  // upload
 	downloadCost := pt.DownloadBandwidthCost.Mul64(expectedDownload)
 	uploadCost := pt.UploadBandwidthCost.Mul64(expectedUpload)
 	bandwidthCost := downloadCost.Add(uploadCost)
