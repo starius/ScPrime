4eea0bc14434657cf4bb76bb43a2072fa672767c
Jay Rama <gitlab@jay.co.nz>
2022-06-02 23:58:54 +0000

    Update release.sh to include LICENSE and README .md in the release package

--- release.sh
+++ release.sh
@@ -53,6 +53,9 @@ for arch in amd64 arm; do
 
 		done
 
+				# add other artifacts
+		cp -r LICENSE README.md $folder
+		# zip
 		(
 			cd release
 			zip -rq ScPrime-$version-$os-$arch.zip ScPrime-$version-$os-$arch
