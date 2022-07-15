c64cb43975a8a202510ec5b9c6931c2f9f6dfc4c
Pavel Dolgov <pdolgov99@gmail.com>
2022-07-15 10:55:59 -0400

    Update version

--- build/version.go
+++ build/version.go
@@ -14,7 +14,7 @@ const (
 	MaxEncodedVersionLength = 100
 
 	// Version is the current version of ScPrime daemon.
-	Version = "1.6.4.1"
+	Version = "1.6.5"
 )
 
 // ReleaseTag contains the release tag, such as "rc3". It is supplied at build
