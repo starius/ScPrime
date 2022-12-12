b8b91b31ea121e6ec6f21c4208a72fdcfeb146e2
Boris <boriscp@protonmail.com>
2022-12-12 02:59:47 +0100

    unburn test: verify sending from a special addr
    in addition to receiving

--- modules/host/unburn_test.go
+++ modules/host/unburn_test.go
@@ -33,86 +33,128 @@ func TestUnburn(t *testing.T) {
 		},
 	}
 
+	burntAmount := types.NewCurrency64(10000000000000000000).Mul(types.NewCurrency64(100000000))
+	thresholdAmount := types.NewCurrency64(10000000000000000000).Mul(types.NewCurrency64(100000))
+
 	for _, tc := range cases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			ht, err := newMockHostTester(modules.ProdDependencies, t.Name())
-			require.NoError(t, err)
+			var (
+				err           error
+				ht            *hostTester
+				encryptionKey crypto.CipherKey
+				seed          modules.Seed
+				addr          types.UnlockConditions
+				wallet2       *wallet.Wallet
+				balance2      types.Currency
+			)
+
+			t.Run("initialize wallets", func(t *testing.T) {
+				ht, err = newMockHostTester(modules.ProdDependencies, t.Name())
+				require.NoError(t, err)
+
+				encryptionKey = crypto.GenerateSiaKey(crypto.TypeDefaultWallet)
+
+				wallet1, err := wallet.New(ht.cs, ht.tpool, filepath.Join(ht.persistDir, modules.WalletDir+"wallet1"))
+				require.NoError(t, err)
+
+				seed, err = wallet1.Encrypt(encryptionKey)
+				require.NoError(t, err)
+				require.NoError(t, wallet1.Unlock(encryptionKey))
+
+				addr, err = wallet1.NextAddress()
+				require.NoError(t, err)
+			})
 			t.Cleanup(func() {
 				require.NoError(t, ht.Close())
 			})
 
-			encryptionKey := crypto.GenerateSiaKey(crypto.TypeDefaultWallet)
-
-			wallet1, err := wallet.New(ht.cs, ht.tpool, filepath.Join(ht.persistDir, modules.WalletDir+"wallet1"))
-			require.NoError(t, err)
-
-			seed, err := wallet1.Encrypt(encryptionKey)
-			require.NoError(t, err)
-			require.NoError(t, wallet1.Unlock(encryptionKey))
+			sync := func(w *wallet.Wallet) {
+				// Wait for the block to propagate to wallet2.
+				wantHeight := ht.cs.Height()
+				for {
+					gotHeight, err := w.Height()
+					require.NoError(t, err)
+					if gotHeight >= wantHeight {
+						break
+					}
+					t.Logf("Waiting, height %d < %d", gotHeight, wantHeight)
+					time.Sleep(time.Second)
+				}
+			}
 
 			// Make some of wallet's addresses spendAddress and activate Fork2022.
-			addr, err := wallet1.NextAddress()
-			require.NoError(t, err)
 			originalSpendAddress := *tc.spendAddress
 			originalFork2022 := types.Fork2022
 			*tc.spendAddress = addr.UnlockHash()
 			types.Fork2022 = true
-			defer func() {
+			t.Cleanup(func() {
 				*tc.spendAddress = originalSpendAddress
 				types.Fork2022 = originalFork2022
-			}()
+			})
 
-			// Recreate the wallet with the same seed.
-			wallet2, err := wallet.New(ht.cs, ht.tpool, filepath.Join(ht.persistDir, modules.WalletDir+"wallet2"))
-			require.NoError(t, err)
-			require.NoError(t, wallet2.InitFromSeed(encryptionKey, seed))
-			require.NoError(t, wallet2.Unlock(encryptionKey))
-
-			// Wait for the block to propagate to wallet2.
-			wantHeight := ht.cs.Height()
-			for {
-				gotHeight, err := wallet2.Height()
+			t.Run("recreate the wallet with the same seed", func(t *testing.T) {
+				wallet2, err = wallet.New(ht.cs, ht.tpool, filepath.Join(ht.persistDir, modules.WalletDir+"wallet2"))
 				require.NoError(t, err)
-				if gotHeight >= wantHeight {
-					break
-				}
-				t.Logf("Waiting, height %d < %d", gotHeight, wantHeight)
-				time.Sleep(time.Second)
-			}
+				require.NoError(t, wallet2.InitFromSeed(encryptionKey, seed))
+				require.NoError(t, wallet2.Unlock(encryptionKey))
+			})
 
-			balance1, _, _, err := wallet2.ConfirmedBalance()
-			require.NoError(t, err)
-			t.Logf("Confirmed balance 1: %s", balance1)
+			t.Run("send coins to special address", func(t *testing.T) {
+				// Wait for the block to propagate to wallet2.
+				sync(wallet2)
 
-			burntAmount := types.NewCurrency64(10000000000000000000).Mul(types.NewCurrency64(100000000))
-
-			_, err = ht.wallet.SendSiacoins(burntAmount, tc.sendAddress)
-			require.NoError(t, err)
-
-			_, err = ht.miner.AddBlock()
-			require.NoError(t, err)
-
-			// Wait for the block to propagate to wallet2.
-			wantHeight = ht.cs.Height()
-			for {
-				gotHeight, err := wallet2.Height()
+				balance1, _, _, err := wallet2.ConfirmedBalance()
 				require.NoError(t, err)
-				if gotHeight >= wantHeight {
-					break
-				}
-				t.Logf("Waiting, height %d < %d", gotHeight, wantHeight)
-				time.Sleep(time.Second)
-			}
+				t.Logf("Confirmed balance 1: %s", balance1)
 
-			balance2, _, _, err := wallet2.ConfirmedBalance()
-			require.NoError(t, err)
-			t.Logf("Confirmed balance 2: %s", balance2)
+				_, err = ht.wallet.SendSiacoins(burntAmount, tc.sendAddress)
+				require.NoError(t, err)
 
-			require.Equal(t, tc.wantBalance, balance2.String())
+				_, err = ht.miner.AddBlock()
+				require.NoError(t, err)
 
-			diff := balance2.Sub(balance1)
-			require.Equal(t, burntAmount.String(), diff.String())
+				// Wait for the block to propagate to wallet2.
+				sync(wallet2)
+
+				balance2, _, _, err = wallet2.ConfirmedBalance()
+				require.NoError(t, err)
+				t.Logf("Confirmed balance 2: %s", balance2)
+
+				require.Equal(t, tc.wantBalance, balance2.String())
+
+				diff := balance2.Sub(balance1)
+				require.Equal(t, burntAmount.String(), diff.String())
+			})
+
+			t.Run("send coins from special address", func(t *testing.T) {
+				wallet3, err := wallet.New(ht.cs, ht.tpool, filepath.Join(ht.persistDir, modules.WalletDir+"wallet3"))
+				require.NoError(t, err)
+				encryptionKey2 := crypto.GenerateSiaKey(crypto.TypeDefaultWallet)
+				_, err = wallet3.Encrypt(encryptionKey2)
+				require.NoError(t, err)
+				require.NoError(t, wallet3.Unlock(encryptionKey2))
+
+				addr, err = wallet3.NextAddress()
+				require.NoError(t, err)
+
+				amount := balance2.Sub(thresholdAmount)
+
+				_, err = wallet2.SendSiacoins(amount, addr.UnlockHash())
+				require.NoError(t, err)
+
+				_, err = ht.miner.AddBlock()
+				require.NoError(t, err)
+
+				// Wait for the block to propagate to wallet2.
+				sync(wallet3)
+
+				balance3, _, _, err := wallet3.ConfirmedBalance()
+				require.NoError(t, err)
+				t.Logf("Confirmed balance 3: %s", balance3)
+
+				require.Equal(t, amount.String(), balance3.String())
+			})
 		})
 	}
 }
